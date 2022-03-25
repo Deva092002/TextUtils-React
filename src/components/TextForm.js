088b54aa0a0cae358adadf502bab5be5dbafd6e7
Deva092002 <92011465+Deva092002@users.noreply.github.com>
2022-03-25 18:34:19 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -81,9 +81,9 @@ export default function TextForm(props) {
       >
         <h1>Your Text Summary</h1>
         <p>
-          {text.split(" ").length} words and {text.length} character
+          {text.split(" ").filter((element)=>{return element.length!= 0}).length} words and {text.length} character
         </p>
-        <p>{0.008 * text.split(" ").length} Minutes to read</p>
+        <p>{0.008 * text.split(" ").filter((element)=>{return element.length!= 0}).length} Minutes to read</p>
         <h2>Preview</h2>
         <p>
           {text.length > 0
